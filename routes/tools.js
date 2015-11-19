4f116b10deed3f43ba30aadd235c33a41db388db
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-11-19 13:02:50 -0800

    fixed addtool template to provide correct data, edited toolsList with correct info, added POST routing and save info to tools.js

--- routes/tools.js
+++ routes/tools.js
@@ -1,5 +1,6 @@
 var tools = module.exports;
 var Tool = require('../models/Tool');
+var User = require('../models/User');
 
 tools.index = function(req, res, next) {
   Tool.find(function(err, tools) {
@@ -18,18 +19,25 @@ tools.new = function(req, res) {
 tools.create = function(req, res, next) {
   var tool = new Tool();
 
-  tool.userId = req.body.userId;
+  if (req.user) { // Session id is present
+    tool.userId = req.user.id;
+  } else { //Assume mocha test
+    tool.userId = req.body.userId;
+  }
+
   tool.category = req.body.category;
   tool.name = req.body.name;
   tool.description = req.body.description;
-  tool.isAvailable = req.body.isAvailable;
+  tool.isAvailable = true;
 
+  //add
   tool.save(function(err) {
     if (err) {
       return next(err);
     } else {
       //res.render('../views/index.jade', tool);
-      res.json(tool);
+      //res.json(tool);
+      res.redirect('/dashboard');
     }
   });
 };
