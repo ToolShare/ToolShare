71e39146fb050d8ab7965a9be43b0986c699a93e
dxeware <dxeware@gmail.com>
2015-11-20 12:37:33 -0800

    jscs/lint fixes

--- routes/findTools.js
+++ routes/findTools.js
@@ -4,40 +4,43 @@ var User = require('../models/User');
 var Tool = require('../models/Tool');
 
 router.get('/:category?', function(req, res, next) {
-  var category = req.params.category ? req.params.category :'power';
+  var category = req.params.category ? req.params.category : 'power';
   options = {
     category: category,
     isAvailable: true,
-  }
+  };
   Tool.find(options)
   .exec(function(err, dbtools) {
     if (req.user) {
-      var filteredTools = dbtools.filter(function(tool){
-        return (tool.userId !== req.user.id)
-      })
-      dbtools = filteredTools
+      var filteredTools = dbtools.filter(function(tool) {
+        return (tool.userId !== req.user.id);
+      });
+
+      dbtools = filteredTools;
     }
+
     var tools = [];
     var toolType = {};
     dbtools.map(function(tool) {
       if (toolType[tool.name]) {
-        toolType[tool.name] += 1
+        toolType[tool.name] += 1;
       } else {
         toolType[tool.name] = 1;
       }
     });
-    for (key in toolType) {
-      tools.push({name:key,count:toolType[key]})
+
+    for (var key in toolType) {
+      tools.push({name:key, count:toolType[key]});
     }
+
     var user = req.user ? req.user : false;
     res.render('findtools', {
       category: category,
       tools: tools,
       cats: require('../helpers/toolsList'),
       user: user,
-    })
+    });
   });
-})
-
+});
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/User');
-var Tool = require('../models/Tool')
+var Tool = require('../models/Tool');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,8 +16,8 @@ router.get('/register', function(req, res, next) {
 });
 
 router.post('/register', function(req, res) {
-    User.register(new User({
-      username : req.body.username,
+  User.register(new User({
+      username: req.body.username,
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       address: {
@@ -25,14 +25,17 @@ router.post('/register', function(req, res) {
         city: req.body.city,
         state: req.body.state,
         zip: req.body.zip,
-      }
+      },
     }), req.body.password, function(err, account) {
-        if (err) {
-          return res.render("register", {info: "Sorry. That username already exists. Try again."});
-        }
-        passport.authenticate('local')(req, res, function () {
-            res.redirect('/dashboard');
+      if (err) {
+        return res.render('register', {
+          info: 'Sorry. That username already exists. Try again.',
         });
+      }
+
+      passport.authenticate('local')(req, res, function() {
+        res.redirect('/dashboard');
+      });
     });
 });
 
@@ -44,12 +47,15 @@ router.get('/login', function(req, res) {
 router.post('/login', function(req, res, next) {
   passport.authenticate('local', function(err, user, info) {
     if (err) { return next(err); }
+
     if (!user) {
-      req.session.loginErr = "Invalid Username or Password";
+      req.session.loginErr = 'Invalid Username or Password';
       return res.redirect('/login');
     }
+
     req.logIn(user, function(err) {
       if (err) { return next(err); }
+
       return res.redirect('/dashboard');
     });
   })(req, res, next);
@@ -60,26 +66,27 @@ router.get('/logout', function(req, res) {
   res.redirect('/');
 });
 
-router.get('/requesttool/:tool', function(req,res, next){
-  if(!req.isAuthenticated()) res.redirect('/login')
+router.get('/requesttool/:tool', function(req, res, next) {
+  if (!req.isAuthenticated()) res.redirect('/login');
   Tool.find({name:req.params.tool, isAvailable: true})
   .populate('_user')
-  .exec(function(err, tools){
-    var filteredTools = tools.filter(function(tool){
-      if (req.user) return (tool.userId !== req.user.id)
-    })
-    res.render('requesttool',{tools:tools, user:req.user})
-  })
+  .exec(function(err, tools) {
+    var filteredTools = tools.filter(function(tool) {
+      if (req.user) return (tool.userId !== req.user.id);
+    });
+
+    res.render('requesttool', {tools:tools, user:req.user});
+  });
 });
 
 router.get('/tools/add', function(req, res, next) {
-  var category = req.query.category ? req.query.category :'power';
+  var category = req.query.category ? req.query.category : 'power';
   var user = req.user ? req.user : false;
   res.render('addtool', {
       category: category,
       lists: require('../helpers/toolsList'),
       user: user,
-    })
+    });
 });
 
 module.exports = router;
