775ce5f2e0313ff59b41f128f2615d495e242442
PREET GUJRAL <PREET@PREETGUJRAL.COM>
2015-11-20 12:27:12 -0800

    linting files in root and directories a-p

--- public/javascripts/putdelete.js
+++ public/javascripts/putdelete.js
@@ -1,25 +1,29 @@
-$(document).ready(function(){
-  $('.request-put').on('click', function(e){
+$(document).ready(function() {
+  $('.request-put').on('click', function(e) {
     e.preventDefault();
     var $this = $(this);
     $.ajax({
       url: $this.attr('href'),
       method: 'PUT',
-      data: {status: $this.attr('data-loan-response'), id:$this.attr('data-req-id')}
-      })
+      data: {
+        status: $this.attr('data-loan-response'),
+        id:$this.attr('data-req-id')
+      },
+    })
       .done(function(data) {
         location.reload();
       });
-  })
-  $('.request-del').on('click', function(e){
+  });
+
+  $('.request-del').on('click', function(e) {
     e.preventDefault();
     var $this = $(this);
     $.ajax({
       url: $this.attr('href'),
       method: 'DELETE',
-      })
+    })
       .done(function(data) {
         location.reload();
       });
-  })
-})
+  });
+});
